d27e87104328fddfac1652d1de094a45302f52ca
garvageart <45718863+garvageart@users.noreply.github.com>
2023-10-12 16:31:45 +0200

    added convertDate and summary generator functions

--- src/plugins/left-2-write/l2w.util.ts
+++ src/plugins/left-2-write/l2w.util.ts
@@ -1,3 +1,22 @@
 export function convertToSentenceCase(text: string) {
     return text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
 }
+
+export const convertDate = (inputDate: Date) => {
+    const date = inputDate;
+    const day = date.getDate();
+    const month = date.toLocaleString("default", { month: "long" });
+    const year = date.getFullYear();
+
+    return `${day} ${month} ${year} — ${date.toLocaleTimeString(
+        "en-ZA",
+        {
+            timeStyle: "short"
+        }
+    )}`;
+};
+
+// Needs some improvement but this is fine for now
+export function generatePostDescription(text: string, maxLength: number) {
+    return text.substring(0, maxLength);
+}
