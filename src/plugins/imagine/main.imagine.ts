74e809ffc1dd0445ac740cd6430e0ab06384da75
garvageart <45718863+garvageart@users.noreply.github.com>
2023-12-29 18:59:01 +0200

    changed if statement to exit process on production

--- src/plugins/imagine/main.imagine.ts
+++ src/plugins/imagine/main.imagine.ts
@@ -4,33 +4,33 @@ import { GeneralLogger } from "../../logger";
 import { DatabaseService } from "../../services/db.service";
 import { ImaginePlugin } from "./imagine_processor";
 
-if (!IS_ENV.production) {
-    const testDatabase = new DatabaseService({
-        schema: Imagine,
-        collectionName: "webflow-images"
-    });
-
-    testDatabase.initializeDatabase({
-        dbName: "ImagineTestDB",
-        uri: process.env.MONGODB_DOMAIN,
-        retryLimit: 3
-    });
-
-    const testImages = new ImaginePlugin({ processingQuality: 30 });
-
-    const directories = testImages.categories;
-
-    const processedImages = await testImages.processor(directories);
-    const failedImages = await testImages.exportImages(processedImages.imageArray);
-
-    testImages.logger.info('Image processing has been completed!');
-
-    if (failedImages.length !== 0) {
-        testImages.logger.error(`These images failed to be processed`, failedImages.map(image => image.metadata));
-    }
-
-    testDatabase.disconnect();
-} else {
-    GeneralLogger.error(`Cannot run plugin main during production`);
+if (IS_ENV.production) {
+    GeneralLogger.warn(`Cannot run plugin main during production`);
     process.exit(1);
 }
+
+const testDatabase = new DatabaseService({
+    schema: Imagine,
+    collectionName: "webflow-images"
+});
+
+testDatabase.initializeDatabase({
+    dbName: "ImagineTestDB",
+    uri: process.env.MONGODB_DOMAIN,
+    retryLimit: 3
+});
+
+const testImages = new ImaginePlugin({ processingQuality: 30 });
+
+const directories = testImages.categories;
+
+const processedImages = await testImages.processor(directories);
+const failedImages = await testImages.exportImages(processedImages);
+
+testImages.logger.info('Image processing has been completed!');
+
+if (failedImages.length !== 0) {
+    testImages.logger.warn(`These images failed to be processed`, failedImages);
+}
+
+testDatabase.disconnect();
