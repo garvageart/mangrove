8ecdf2480ce93e1ea79c59b87528da71e86aba7f
garvageart <45718863+garvageart@users.noreply.github.com>
2023-12-29 18:49:27 +0200

    Add preview server options port

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
+/// <reference types="vitest" />
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, } from 'vite';
+
 
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
+		port: 5555,
+		cors: true
+	},
+	preview: {
 		port: 5555,
 		cors: true
 	}
