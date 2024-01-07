59216ebd5d02a96ece19f106524beb5cbda55ec1
garvageart <45718863+garvageart@users.noreply.github.com>
2024-01-07 16:17:05 +0200

    try all transport methods

--- src/plugins/left-2-write/client/lib/stores/socket.ts
+++ src/plugins/left-2-write/client/lib/stores/socket.ts
@@ -3,7 +3,7 @@ import { L2W_EDITING_SERVER_HREF } from "../../../l2w.constants";
 import { readable } from "svelte/store";
 
 const socketConnection = io(L2W_EDITING_SERVER_HREF, {
-	transports: ["polling"]
+	transports: ["websocket", 'polling', "webtransport"]
 });
 
 export const socketStore = readable(socketConnection) 
