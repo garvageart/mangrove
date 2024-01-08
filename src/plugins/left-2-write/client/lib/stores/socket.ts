a034ef2f867126e073748d0b3365576bb76a9e39
garvageart <45718863+garvageart@users.noreply.github.com>
2024-01-08 23:14:48 +0200

    fix socket url (again lmao)

--- src/plugins/left-2-write/client/lib/stores/socket.ts
+++ src/plugins/left-2-write/client/lib/stores/socket.ts
@@ -1,9 +1,9 @@
 import { io } from "socket.io-client";
-import { L2W_SERVER_HREF } from "../../../l2w.constants";
+import { L2W_SERVER_HREF, L2W_SERVER_PORT, L2W_SERVER_URL } from "../../../l2w.constants";
 import { readable } from "svelte/store";
 import { browser } from "$app/environment";
 
-const socketConnection = io((!browser && !globalThis.window?.location !== undefined && !globalThis.window?.location?.port) ? L2W_SERVER_HREF.replace('http', '') : `${location?.hostname}:3333`, {
+const socketConnection = io((browser && !location?.port) ? `${L2W_SERVER_URL.replace('http', 'https')}:${L2W_SERVER_PORT}` : L2W_SERVER_HREF, {
 	transports: ["websocket", 'polling', "webtransport"]
 });
 
