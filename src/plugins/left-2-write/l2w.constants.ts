4190341bbc636a2e25118b310c7989f3188da8e8
garvageart <45718863+garvageart@users.noreply.github.com>
2024-01-08 02:33:48 +0200

    determine backend server href

--- src/plugins/left-2-write/l2w.constants.ts
+++ src/plugins/left-2-write/l2w.constants.ts
@@ -1,10 +1,12 @@
+import { IS_ENV } from "../../globals";
+
 export const L2W_EDITING_SERVER_PORT = 7777;
 export const L2W_EDITING_SERVER_URL = 'http://leaf-live-editor.lesis.online';
 export const L2W_EDITING_SERVER_HREF = `${L2W_EDITING_SERVER_URL}:${L2W_EDITING_SERVER_PORT}`;
 
 export const L2W_SERVER_PORT = 3333;
 export const L2W_SERVER_URL = 'http://leaf.lesis.online';
-export const L2W_SERVER_HREF = `${L2W_SERVER_URL}:${L2W_SERVER_PORT}`;
+export const L2W_SERVER_HREF = (IS_ENV.production || !window.location.port)? `${L2W_SERVER_URL}:${L2W_SERVER_PORT}` : `https://127.0.0.1:${L2W_SERVER_PORT}`;
 
 export const L2W_EDITOR_PORT = 5555;
 export const L2W_EDITOR_URL = 'http://leaf-editor.lesis.online';
