39c2ffdf5509e4c010090dbc186c359fc4bfc3e6
garvageart <45718863+garvageart@users.noreply.github.com>
2024-01-10 14:47:51 +0200

    conditionally determine hook urls

--- src/plugins/left-2-write/client/hooks.client.ts
+++ src/plugins/left-2-write/client/hooks.client.ts
@@ -4,8 +4,11 @@ import { L2W_SERVER_PORT } from "../l2w.constants";
 export const handleFetch: HandleFetch = async ({ request, fetch }) => {
     if (request.url.includes('.lesis.online')) {
         const reqURL = new URL(request.url);
+        const newURLbase = `http://127.0.0.1:${L2W_SERVER_PORT}${reqURL.pathname}`;
+        const newURL = !globalThis.window?.location.port ? newURLbase.replace('http', 'https') : newURLbase;
+
         request = new Request(
-            `http://127.0.0.1:${L2W_SERVER_PORT}${reqURL.pathname}`,
+            newURL,
             request,
         );
     }
--- src/plugins/left-2-write/client/hooks.server.ts
+++ src/plugins/left-2-write/client/hooks.server.ts
@@ -1,11 +1,15 @@
 import type { HandleFetch } from '@sveltejs/kit';
 import { L2W_SERVER_PORT } from "../l2w.constants";
+import { IS_ENV } from "../../../globals";
 
 export const handleFetch: HandleFetch = async ({ request, fetch }) => {
     if (request.url.includes('.lesis.online')) {
         const reqURL = new URL(request.url);
+        const newURLbase = `http://127.0.0.1:${L2W_SERVER_PORT}${reqURL.pathname}`;
+        const newURL = !IS_ENV.development ? newURLbase.replace('http', 'https') : newURLbase;
+        
         request = new Request(
-            `http://127.0.0.1:${L2W_SERVER_PORT}${reqURL.pathname}`,
+            newURL,
             request,
         );
     }
