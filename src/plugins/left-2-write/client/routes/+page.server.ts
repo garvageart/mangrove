83b3028872eefdab6b1da357bbc6bf93e642a333
garvageart <45718863+garvageart@users.noreply.github.com>
2024-01-10 16:18:08 +0200

    unused IS_ENV import

--- src/plugins/left-2-write/client/routes/+page.server.ts
+++ src/plugins/left-2-write/client/routes/+page.server.ts
@@ -1,6 +1,5 @@
 import { dev } from "$app/environment";
 import { PUBLIC_WEBSITE_STAGING_DOMAIN_NAME } from "$env/static/public";
-import { IS_ENV } from "../../../../globals";
 import type { ILeft2Write } from "../../../../types/plugins/l2w.types";
 import { L2W_SERVER_HREF } from "../../l2w.constants";
 import type { PageServerLoad } from "./$types";
